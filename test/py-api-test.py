85f8a55aaebc071a113bc780f3a9b48d6d2b63d5
Dane Springmeyer <dane@dbsgeo.com>
2012-08-02 15:36:48 -0700

    updates to catch more exceptions

---
+++ test/py-api-test.py
@@ -0,0 +1,8 @@
+from mapnik_c import *
+
+mapnik_register_datasources("/usr/local/lib/mapnik/input")
+m = mapnik_map(256,256)
+mapnik_map_load(m,"sample/stylesheet.xml")
+mapnik_map_zoom_all(m)
+mapnik_render_to_file(m,"py-test.png")
+mapnik_map_free(m)
